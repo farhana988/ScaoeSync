3c76b2bd496af8efd71ab247a1852912705c0f67
farhana988 <farhana744255@gmail.com>
2025-09-24 17:34:28 +0600

    folder structure and installation

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
+
+
+const MainLayout = () => {
+
+  return (
+    <>
+      <div className="max-w-[1600px] mx-auto shadow-2xl">
+        <Navbar />
+    
+        <div className="container mx-auto mt-10 px-5 xl:px-0 min-h-screen">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+export default router;
